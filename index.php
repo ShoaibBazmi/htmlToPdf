d7e44726662ebc7f4283e46bc796dad61c0f5c30
Zain Musani <zaki.raza@viabletree.com>
2022-01-28 22:37:46 +0500

    pdf

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
 
         include_once './mpdf/mpdf.php';
 
-		//https://mpdf.github.io/reference/mpdf-functions/construct.html
+		// https://mpdf.github.io/reference/mpdf-functions/construct.html
 		$html = file_get_contents('./index.html');
 		echo $html;
-		$pdf = new mPDF('L','','','','',25,25,55,45,18,12);
-		$pdf->SetDisplayMode('fullpage');
+		$pdf = new mPDF('A0','','','','',25,25,55,45,18,12);
+        // var_dump($pdf); die;
+		// $pdf->SetDisplayMode('fullpage');
         $pdf->showImageErrors = true;
         //generate the PDF!
         $pdf->WriteHTML($html);
